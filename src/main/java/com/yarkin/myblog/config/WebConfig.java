91ff70f4d81218b3da09c2e99608a05e16f34d09
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-07-10 20:08:20 +0300

    Allow cors for my frontend application

---
+++ src/main/java/com/yarkin/myblog/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.yarkin.myblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * TODO: add class / interface description
+ *
+ * @author OYarkin
+ * @since 8.3.0
+ */
+@Configuration
+public class WebConfig
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings(CorsRegistry registry)
+            {
+                registry
+                    .addMapping("/api/v1/**")
+                    .allowedOrigins("https://oyarkin-blog.herokuapp.com");
+            }
+        };
+    }
+}
